d4b731db405f379432c16ae5d8783d645c82c64d
EdgarFuentes100 <edgarfuentes139@gmail.com>
2024-11-12 09:23:25 -0800

    Cambios en categorias para poder darle siguiente

--- Views/Tienda/categoria.php
+++ Views/Tienda/categoria.php
@@ -19,6 +19,25 @@ $currentCategory = $data['infoCategoria']['categoria'];
 					<?= $currentCategory ?>
 				</div>
 			</div>
+			<div class="flex-w flex-c-m m-tb-10">
+				<?php
+				if (count($data['productos']) > 0) {
+					$prevPagina = $data['pagina'] - 1;
+					$nextPagina = $data['pagina'] + 1; ?>
+					<div class="pagination-buttons">
+						<?php if ($data['pagina'] > 1) { ?>
+							<a href="<?= base_url() ?>/tienda/categoria/<?= $data['infoCategoria']['idcategoria'] . '/' . $data['infoCategoria']['ruta'] . '/' . $prevPagina ?>"
+								class="pagination-button">
+								<i class="fas fa-chevron-left"></i> Anterior</a>
+						<?php } ?>
+						<?php if ($data['pagina'] != $data['total_paginas']) { ?>
+							<a href="<?= base_url() ?>/tienda/categoria/<?= $data['infoCategoria']['idcategoria'] . '/' . $data['infoCategoria']['ruta'] . '/' . $nextPagina ?>"
+								class="pagination-button">
+								Siguiente <i class="fas fa-chevron-right"></i></a>
+						<?php } ?>
+					</div>
+				<?php } ?>
+			</div>
 
 			<div class="dis-none panel-filter w-full p-t-10 mb-3">
 				<div class="wrap-filter flex-w bg6 w-full p-lr-40 p-t-27 p-lr-15-sm">
@@ -90,27 +109,14 @@ $currentCategory = $data['infoCategoria']['categoria'];
 				<p>No hay productos para mostrar <a href="<?= base_url() ?>/tienda"> Ver productos</a></p>
 			<?php } ?>
 		</div>
-
-		<!-- Load more -->
-		<?php
-		if (count($data['productos']) > 0) {
-			$prevPagina = $data['pagina'] - 1;
-			$nextPagina = $data['pagina'] + 1; ?>
-			<div class="flex-c-m flex-w w-full p-t-45">
-				<?php if ($data['pagina'] > 1) { ?>
-					<a href="<?= base_url() ?>/tienda/page/<?= $prevPagina ?>" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04"><i class="fas fa-chevron-left"></i> &nbsp; Anterior</a>&nbsp;&nbsp;
-				<?php } ?>
-				<?php if ($data['pagina'] != $data['total_paginas']) { ?>
-					<a href="<?= base_url() ?>/tienda/page/<?= $nextPagina ?>" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">Siguiente &nbsp; <i class="fas fa-chevron-right"></i></a>
-				<?php } ?>
-			</div>
-		<?php } ?>
 	</div>
 </div>
 
 <style>
 	/* Estilos generales */
-	.filter-button {
+	/* Estilo para el filtro y paginación */
+	.filter-button,
+	.pagination-button {
 		background-color: #624E88;
 		color: white;
 		padding: 10px 15px;
@@ -125,15 +131,29 @@ $currentCategory = $data['infoCategoria']['categoria'];
 		box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
 	}
 
-	.filter-button:hover {
+	.filter-button:hover,
+	.pagination-button:hover {
 		background-color: #7A61B0;
 		color: #ffffff;
 		transform: scale(1.05);
 		border: 2px solid #7A61B0;
 	}
 
-	.filter-button:active {
+	.filter-button:active,
+	.pagination-button:active {
 		transform: scale(0.95);
+	}
+
+	/* Paginación en la misma fila que el filtro */
+	.pagination-buttons {
+		display: flex;
+		justify-content: flex-end;
+		gap: 10px;
+	}
+
+	.pagination-button i {
+		margin-left: 5px;
+		margin-right: 5px;
 	}
 
 	/* Estilos para las categorías */
